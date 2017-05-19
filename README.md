c0e8f1d0b7bdffa248c12286b58660e7629a2fe0
Rudolf Bargholz <rudolf@bargholz.ch>
2017-05-19 23:04:01 +0200

    Savepoint

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fedora-install
 Clones numerous github repositories, and executes the appropriate scripts that install postgres, elixir, erlang, elm, etc.
+
+```shell
+cd fedora-install
+chmod +x install.sh
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+dnf install nano git -y
+
+declare CWD=$(pwd)
+declare TEMPDIRECTORY=$(mktemp --directory)
+
+# postgres
+cd "$TEMPDIRECTORY"
+git clone https://github.com/rudolfb/fedora-postgres-install.git
+cd fedora-postgres-install
+chmod +x postgres.sh
+./postgres.sh
+
+sublime text 3 dev
+cd "$TEMPDIRECTORY"
+git clone https://github.com/rudolfb/sublime-text-3-install.git
+cd sublime-text-3-install
+chmod +x sublime-text-3.sh
+./sublime-text-3.sh -d
+
+elixir, erlang, elm
+cd "$TEMPDIRECTORY"
+git clone https://github.com/rudolfb/fedora-elixir-erlang-install.git
+cd fedora-elixir-erlang-install
+chmod +x elixir-erlang.sh
+./elixir-erlang.sh
+
+
+
+
+cd "$CWD"
+
+# Clean up the dynamically created temp directory.
+if [ -d "$TEMPDIRECTORY" ]; then
+    sudo rm -r "$TEMPDIRECTORY"
+fi
